2180b006e77112aa1efa505a5115d2ebe5ffb6a7
amazingguni <amazingguni@gmail.com>
2023-01-24 16:20:03 +0900

    Finish lab 2b

--- text_recognizer/data/__init__.py
+++ text_recognizer/data/__init__.py
@@ -1,3 +1,5 @@
 from .base_data_module import BaseDataModule
+from .emnist import EMNIST
+from .emnist_lines import EMNISTLines
 from .mnist import MNIST
 from .util import BaseDataset
--- text_recognizer/models/__init__.py
+++ text_recognizer/models/__init__.py
@@ -1,1 +1,3 @@
+from .cnn import CNN
+from .line_cnn_simple import LineCNNSimple
 from .mlp import MLP
