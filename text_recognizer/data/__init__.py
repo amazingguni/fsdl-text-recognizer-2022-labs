29dd4e9e23db40c547b5832c63db2309798de58a
amazingguni <amazingguni@gmail.com>
2023-01-28 20:57:13 +0900

    Finish lab 06

--- text_recognizer/data/__init__.py
+++ text_recognizer/data/__init__.py
@@ -5,5 +5,6 @@ from .fake_images import FakeImageData
 from .iam import IAM
 from .iam_lines import IAMLines
 from .iam_paragraphs import IAMParagraphs
+from .iam_synthetic_paragraphs import IAMSyntheticParagraphs
 from .mnist import MNIST
 from .util import BaseDataset
