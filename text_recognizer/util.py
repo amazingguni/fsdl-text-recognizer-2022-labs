065f888a46174f30944c29f55fbeadec1d9b1d7f
amazingguni <amazingguni@gmail.com>
2023-02-12 15:17:50 +0900

    Finish lab07

--- text_recognizer/util.py
+++ text_recognizer/util.py
@@ -1,14 +1,16 @@
 """Utility functions for text_recognizer module."""
+import base64
 import contextlib
 import hashlib
 import os
+from io import BytesIO
 from pathlib import Path
 from typing import Union
 from urllib.request import urlretrieve
 
 import numpy as np
-from PIL import Image
 import smart_open
+from PIL import Image
 from tqdm import tqdm
 
 
@@ -40,6 +42,56 @@ def temporary_working_directory(working_dir: Union[str, Path]):
         yield
     finally:
         os.chdir(curdir)
+
+
+# Hide lines below until Lab 08
+def read_b64_image(b64_string, grayscale=False):
+    """Load base64-encoded images."""
+    try:
+        image_file = read_b64_string(b64_string)
+        return read_image_pil_file(image_file, grayscale)
+    except Exception as exception:
+        raise ValueError("Could not load image from b64 {}: {}".format(b64_string, exception)) from exception
+
+
+def read_b64_string(b64_string, return_data_type=False):
+    """Read a base64-encoded string into an in-memory file-like object."""
+    data_header, b64_data = split_and_validate_b64_string(b64_string)
+    b64_buffer = BytesIO(base64.b64decode(b64_data))
+    if return_data_type:
+        return get_b64_filetype(data_header), b64_buffer
+    else:
+        return b64_buffer
+
+
+def get_b64_filetype(data_header):
+    """Retrieves the filetype information from the data type header of a base64-encoded object."""
+    _, file_type = data_header.split("/")
+    return file_type
+
+
+def split_and_validate_b64_string(b64_string):
+    """Return the data_type and data of a b64 string, with validation."""
+    header, data = b64_string.split(",", 1)
+    assert header.startswith("data:")
+    assert header.endswith(";base64")
+    data_type = header.split(";")[0].split(":")[1]
+    return data_type, data
+
+
+# Hide lines above until Lab 08
+# Hide lines below until Lab 07
+
+
+def encode_b64_image(image, format="png"):
+    """Encode a PIL image as a base64 string."""
+    _buffer = BytesIO()  # bytes that live in memory
+    image.save(_buffer, format=format)  # but which we write to like a file
+    encoded_image = base64.b64encode(_buffer.getvalue()).decode("utf8")
+    return encoded_image
+
+
+# Hide lines above until Lab 07
 
 
 def compute_sha256(filename: Union[Path, str]):
